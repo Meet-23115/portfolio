8c7a280517af6f5999d996b479dc6015e1f948ad
Meet Gelothra <imailmeet4000@gmail.com>
2024-11-18 18:15:11 +0530

    url

--- components/MainSection/Hero.tsx
+++ components/MainSection/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <>
       <div className='h-[100vh]  flex w-full pl-10 pt-5 bottom-round relativec '>
-        <img src="./blackBackgroundImage.jpg " className=' absolute h-full  top-0 left-0 w-full z-[-1]' alt="" />
+        <img src="https://res.cloudinary.com/dgvpslg33/image/upload/v1731933858/blackBackgroundImage_usqr61.jpg" className=' absolute h-full  top-0 left-0 w-full z-[-1]' alt="" />
         <HeroLeftText/>
         <IphoneImage/>
 
